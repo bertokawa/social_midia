dac19abda4f433dcd33e3af4b8724d8fe1a061a3
Berto <humberto.kawa@hotmail.com>
2017-08-26 10:46:33 -0300

    A20170826
    First Commit

---
+++ application/views/header.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	    <title>INSPINIA | Dashboard</title>
+
+	    <link href="<?php echo base_url("assets/css/bootstrap.min.css")?>" rel="stylesheet">
+	    <link href="<?php echo base_url("assets/css/font-awesome.css")?>" rel="stylesheet">
+
+	    <!-- Toastr style -->
+	    <link href="<?php echo base_url("assets/css/plugins/toastr/toastr.min.css")?>" rel="stylesheet">
+
+	    <!-- Gritter -->
+	    <link href="<?php echo base_url("assets/js/plugins/gritter/jquery.gritter.css")?>" rel="stylesheet">
+
+	    <link href="<?php echo base_url("assets/css/animate.css")?>" rel="stylesheet">
+	    <link href="<?php echo base_url("assets/css/style.css")?>" rel="stylesheet">
+
+	</head>
+
+</html>
+
